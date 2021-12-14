d9c51bcb051e9bbbef85e33e600b9d59e7365ca4
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-12-14 21:53:38 +0200

    Extends parent's constructar by adding @specialization and a parameter for it

--- teacher.rb
+++ teacher.rb
@@ -1,3 +1,12 @@
-class Teacher
+require_relative('./person')
 
+class Teacher < Person
+  def initialize(@specialization)
+    super(name, age, parent_permission)
+    @@specialization = @specialization
+  end
+  
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
 end
