89b34ee1c467d0f909ca56425cb8851246c5c6a5
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-12-14 19:57:43 +0200

    Use shortcuts: attr_accessor

--- main.rb
+++ main.rb
@@ -1,32 +1,12 @@
 class Person
   def initialize(name = "Unknown", age, parent_permission = true)
+    @id = Random.rand(1..1000)
     @name = name
     @age = age
+    @parent_permission = parent_permission
   end
 
   attr_reader :id
-  attr_reader :name
-  attr_writer :name
-  attr_reader :age
-  attr_writer :age
-
-  # def id
-  #   @id
-  # end
-
-  # def name
-  #   @name
-  # end
-
-  # def age
-  #   @age
-  # end
-
-  # def name=(value)
-  #   @name = value
-  # end
-
-  #  def age=(value)
-  #   @age = value
-  # end
+  attr_accessor :name
+  attr_accessor :age
 end
