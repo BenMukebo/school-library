0b613387b3f1f46f576c54a5a9aaa0ecef98a348
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-12-17 13:01:15 +0200

    Define the entry point method that is invoked at the end of the file

--- main.rb
+++ main.rb
@@ -1,5 +1,23 @@
-def main
+require_relative 'book'
+require_relative 'teacher'
+require_relative 'student'
+require_relative 'rental'
 
+class App
+  attr_accessor :books, :persons, :rentals
+
+  def initialize
+    @persons = []
+    @books = []
+    @rentals = []
+  end
+end
+
+def main
+  puts 'Welcome to School Library App!'
+  puts
+  app = App.new()
+  app.run()
 end
 
 main()
