023d78f89f589660898948d51903694da897a36b
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-12-17 14:24:00 +0200

    Create a select_options method to display options and give the ability to enter your choosen option

--- main.rb
+++ main.rb
@@ -1,4 +1,7 @@
+#!/usr/bin/env ruby
+
 require_relative 'book'
+# require_relative 'people'
 require_relative 'teacher'
 require_relative 'student'
 require_relative 'rental'
@@ -11,6 +14,25 @@ class App
     @books = []
     @rentals = []
   end
+
+
+
+  def select_options
+    puts 'Please choose an option by entering a number: '
+    options = [
+      '1 - List all books',
+      '2 - List all people',
+      '3 - Create a person',
+      '4 - Create a book',
+      '5 - Create a rental',
+      '6 - List all rentals for a given person id',
+      '7 - Exit'
+    ]
+    puts
+    puts options
+    gets.chomp.to_i
+  end
+
 end
 
 def main
